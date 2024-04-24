7ded376937a7fb86503a6a020f24a731fdf3fa50
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2024-04-24 11:58:21 +0530

    hide mongo url🔥🔥🔥

--- back-end/dbConnection.js
+++ back-end/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
   try{
-    await mongoose.connect('mongodb+srv://Auth:qwerty2801@next-auth.p0ecaww.mongodb.net/bookMyShow');
+    await mongoose.connect('/bookMyShow');
     console.log('Connected to Database')}
   catch(error){
     console.log(error)}
